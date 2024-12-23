255c39f2211d96827c205e126042d8eb19187d8d
Siliciumo2 <147613900+siliciumo2@users.noreply.github.com>
2024-12-23 10:29:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int money = 15;
+        int price = 1;
+        int wrap = 3;
+
+        int totalChocolates = CalculateMaxChocolates(money, price, wrap);
+
+        Console.WriteLine($"Максимально возможное количество шоколадок: {totalChocolates}");
+    }
+
+    static int CalculateMaxChocolates(int money, int price, int wrap)
+    {
+
+        int chocolates = money / price;
+        int wrappers = chocolates;
+
+
+        while (wrappers >= wrap)
+        {
+
+            int newChocolates = wrappers / wrap;
+            chocolates += newChocolates;
+            wrappers = newChocolates + (wrappers % wrap);
+        }
+
+        return chocolates;
+    }
+}
